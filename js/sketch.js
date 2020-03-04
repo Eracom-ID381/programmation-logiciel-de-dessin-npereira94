bb7bbb9ffacbc929772d699371bea33ac9f81168
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-03-04 13:50:27 +0100

    back to the beginning

--- js/sketch.js
+++ js/sketch.js
@@ -5,20 +5,11 @@ function setup() {
 }
 
 function draw() {
-
-  fill(random(0, 255), random(0, 255), 255);
-  ellipse(mouseX, mouseY, 50, 50);
+  fill(255);
+  rect(mouseX, mouseY, 50, 50);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   background(0);
-}
-
-function mousePressed() {
-
-
-  ellipse(mouseX, mouseY, mouseX, mouseY);
-
-
 }
