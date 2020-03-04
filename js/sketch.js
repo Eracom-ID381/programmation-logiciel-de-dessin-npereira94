60d236720f5f57dcaeb6956c2ab215749b9f9f46
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-03-04 14:59:20 +0100

    if in if

--- js/sketch.js
+++ js/sketch.js
@@ -1,20 +1,29 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  noStroke();
+
+
 }
 
 function draw() {
 
-
-
-  stroke(255);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-
+  if (mouseIsPressed && mouseY > height / 2) {
+    ellipse(mouseX, mouseY, 50, 50);
   } else {
-    rect(25, 25, 50, 50);
+    stroke(255)
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
+  /*if (mouseIsPressed) {
+      stroke(255, 23, 40);
+      strokeWeight(10);
+      if (mouseY > height / 2) {
+        fill(255, 23, 40);
+        noStroke();
+        circle(mouseX, mouseY, 10, 10);
+      } else {
+        line(mouseX, mouseY, pmouseX, pmouseY);
+      }
+    }*/
 
 }
 
