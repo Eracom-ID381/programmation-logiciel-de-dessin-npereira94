2ab0d9ba5afa41447c959ea0b3e51615f4fbdb00
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-03-04 15:08:13 +0100

    mousePressed line to circle

--- js/sketch.js
+++ js/sketch.js
@@ -7,26 +7,32 @@ function setup() {
 
 function draw() {
 
-  if (mouseIsPressed && mouseY > height / 2) {
+  /*if (mouseIsPressed && mouseY > height / 2) {
     ellipse(mouseX, mouseY, 50, 50);
   } else {
     stroke(255)
     line(mouseX, mouseY, pmouseX, pmouseY);
+  } */
+  if (mouseIsPressed) {
+    stroke(255, 23, 40);
+    strokeWeight(40);
+    if (mouseY > height / 2) {
+      fill(50, 100, 250);
+      noStroke();
+      circle(mouseX, mouseY, 150, 150);
+    } else {
+      line(mouseX, mouseY, pmouseX, pmouseY);
+    }
   }
-  /*if (mouseIsPressed) {
-      stroke(255, 23, 40);
-      strokeWeight(10);
-      if (mouseY > height / 2) {
-        fill(255, 23, 40);
-        noStroke();
-        circle(mouseX, mouseY, 10, 10);
-      } else {
-        line(mouseX, mouseY, pmouseX, pmouseY);
-      }
-    }*/
+  if (mousePressed) {
 
+    strokeWeight(40);
+    if (mouseY < height / 2) {
+      fill(250);
+      circle(mouseX, mouseY, 150, 150);
+    }
+  }
 }
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
