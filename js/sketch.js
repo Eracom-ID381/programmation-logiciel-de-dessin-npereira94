380c01863f7bd55a2f3e2b74c321ffa5a18d69cf
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-03-11 15:02:08 +0100

    shade line from center

--- js/sketch.js
+++ js/sketch.js
@@ -1,34 +1,37 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  let value = 0;
+
 
 
 }
 
 function draw() {
 
-  fill(random(0, 255), random(0, 255), 255);
-  line(0, mouseY, width, 100);
-  fill(255);
+
+
+
+
   if (mouseIsPressed) {
-    stroke(255);
+    stroke(255, 20, );
     strokeWeight(10);
-    if (mouseY > height / 2) {
-      fill(255, 255, 40);
-      noStroke();
-      circle(mouseX, mouseY, 150, 150);
-    } else {
-      line(mouseX, mouseY, pmouseX, pmouseY);
+
+    if (mouseY < height / 2) {
+      (random(0, 255), random(0, 255), 255);
+      line(mouseX, mouseY, width / 2, height / 2);
+      fill(random(0, 255), random(0, 255), 255);
+      stroke(255, 200, );
+      strokeWeight(1);
     }
   }
 }
-/*
+
+
 function mouseClicked() {
   ellipse(mouseX, mouseY, 5, 5);
   // prevent default
   return false;
-}*/
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
