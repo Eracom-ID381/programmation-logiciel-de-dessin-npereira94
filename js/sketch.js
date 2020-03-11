5bc24428ad2055a77faf8edaecf83c3e59f3a1eb
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-03-11 14:14:11 +0100

    change rect with line

--- js/sketch.js
+++ js/sketch.js
@@ -8,18 +8,14 @@ function setup() {
 
 function draw() {
 
-  /*if (mouseIsPressed && mouseY > height / 2) {
-    ellipse(mouseX, mouseY, 50, 50);
-  } else {
-    stroke(255)
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  } */
-  fill(value);
+  fill(random(0, 255), random(0, 255), 255);
+  line(0, mouseY, width, 100);
+  fill(255);
   if (mouseIsPressed) {
-    stroke(255, 23, 40);
-    strokeWeight(40);
+    stroke(255);
+    strokeWeight(10);
     if (mouseY > height / 2) {
-      fill(50, 100, 250);
+      fill(255, 255, 40);
       noStroke();
       circle(mouseX, mouseY, 150, 150);
     } else {
@@ -27,12 +23,12 @@ function draw() {
     }
   }
 }
-
+/*
 function mouseClicked() {
   ellipse(mouseX, mouseY, 5, 5);
   // prevent default
   return false;
-}
+}*/
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
