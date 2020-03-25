0193aaaf225137ed620a80abbb0a38582f7ed699
Simon Pinkas <simon@code-think.com>
2020-03-25 13:51:44 +0100

    Fixed mode system and soundfile play and pause

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,8 @@
 let mySound;
+let mode = 0;
 
 function preload() {
-  soundFormats('mp3', 'ogg');
+  // soundFormats('mp3', 'ogg');
   mySound = loadSound('audio/Bullit.mp3');
 }
 
@@ -36,45 +37,59 @@ function draw() {
 
     }
   }
+
+
   /*Normal Mode*/
 
-  if (keyCode === LEFT_ARROW) {
+  // Default and Left Arrow
+  if (mode === 0) {
     background(0);
     strokeWeight(2);
     stroke(random(0, 255), random(0, 255), 255);
     line(mouseX, mouseY, 2000, 10);
-    mySound.play();
-  } else {
-
-    mySound.stop();
-
   }
-  /*StrokeWeight*/
-
-  if (keyCode === RIGHT_ARROW) {
+  // Right Arrow
+  else if (mode === 1) {
     background(0);
     background('#0f0');
     strokeWeight(5);
     stroke(random(0, 255), random(0, 255), 255);
     line(mouseX, mouseY, pmouseX, pmouseY);
-
   }
-  /*Christmas Mode*/
-  if (keyCode === DOWN_ARROW) {
+  // Up Arrow
+  else if (mode === 2) {
     background(250, 0, 0);
     stroke(255);
     strokeWeight(2);
     line(mouseX, mouseY, pmouseX, pmouseY);
 
   }
-  if (keyCode === ENTER) {
-
+  // Down Arrow
+  else if (mode === 3) { 
 
   }
+
 }
 
+function mousePressed() { 
+  mySound.play();
+}
+
+function mouseReleased() {
+  mySound.pause();
+}
 
 function keyPressed() {
+  mySound.stop();
+  if (keyCode === LEFT_ARROW) {
+    mode = 0;
+  } else if (keyCode === RIGHT_ARROW) {
+    mode = 1;
+  } else if (keyCode === UP_ARROW) {
+    mode = 2;
+  } else if (keyCode === DOWN_ARROW) { 
+    mode = 3;
+  }
 
   if (keyCode === BACKSPACE) {
     background(0);
